29d7b40693e625a4aeea9d773e2acf6191998f40
tildejustin <spacepepper3.14@gmail.com>
2023-03-16 19:12:21 -0400

    fix and update gradle

--- src/main/java/me/bluesmoke/mangodfps/mixin/GameRendererMixin.java
+++ src/main/java/me/bluesmoke/mangodfps/mixin/GameRendererMixin.java
@@ -1,7 +1,7 @@
 package me.bluesmoke.mangodfps.mixin;
 
 import me.bluesmoke.mangodfps.DynamicMenuFPSMod;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,11 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
     @Shadow
-    private MinecraftClient client;
+    private Minecraft client;
 
     //  1.7.x - method_1331
     //  1.8-1.12.2 - method_9775
-    @Inject(method = {"method_1331", "method_9775"}, at = @At("HEAD"), cancellable = true, remap = false)
+    @Inject(method = "method_1331", at = @At("HEAD"), cancellable = true, remap = false)
     private void onRender(CallbackInfo ci) {
         if (this.client.currentScreen instanceof GameMenuScreen && !DynamicMenuFPSMod.checkForRender()) {
             ci.cancel();
